91ed5877065fe03c23c7c066af787e1899c38432
Mani Movassagh Ghazani <mani.mgh@gmail.com>
2021-07-19 08:43:01 +0200

    main comment added

--- src/main/java/com/example/restfullapp/RestFullApplication.java
+++ src/main/java/com/example/restfullapp/RestFullApplication.java
@@ -3,11 +3,15 @@ package com.example.restfullapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * This is the main Part of Application
+ * With help of SpringBootApplication annotation spring knows how to start the app
+ */
 @SpringBootApplication
 public class RestFullApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestFullApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RestFullApplication.class, args);
+    }
 
 }
